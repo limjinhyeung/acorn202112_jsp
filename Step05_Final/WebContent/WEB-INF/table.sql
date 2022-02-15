5d429aae6e0b603a2b0f4279de7c764caf24fe4e
limjin <iphone9266@naver.com>
2022-02-15 11:38:17 +0900

    textarea

--- Step05_Final/WebContent/WEB-INF/table.sql
+++ Step05_Final/WebContent/WEB-INF/table.sql
@@ -6,3 +6,15 @@ CREATE TABLE users(
 	profile VARCHAR2(100), --프로필 이미지 경로를 지정할 칼럼
 	regdate DATE
 );
+
+-- 게시글을 저장할 테이블
+CREATE TABLE board_cafe(
+	num NUMBER primary key,
+	writer varchar2(100) not null,
+	title varchar2(100)	not null,
+	content clob,
+	viewCount	Number,
+	regdate date
+	);
+--게시글의 번호를 얻어낼 시퀀스
+CREATE SEQUENCE board_cafe_seq;
